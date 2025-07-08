b7c8c32c7cc9ec2926b7c8eb7abb4eaa61908277
Mingpoo <soonmingpoo@yahoo.com>
2025-07-08 10:25:40 +0800

    update

--- DesktopPet-Portable-EXE/DesktopPet.bat
+++ DesktopPet-Portable-EXE/DesktopPet.bat
@@ -1,19 +1,18 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set "APP_DIR=%~dp0"
-set "JAR_FILE=%APP_DIR%lib\AdvancedDesktopPet.jar"
-set "JRE_DIR=%APP_DIR%jre"
+cd /d "%~dp0"
+
+set "JAVA_EXE=jre\bin\java.exe"
+set "JAR_FILE=lib\AdvancedDesktopPet.jar"
 
 echo Desktop Pet Launcher
 echo ===================
 echo.
 
-if exist "%JRE_DIR%\bin\java.exe" (
+if exist "%JAVA_EXE%" (
     echo [INFO] Using embedded JRE - no Java installation required!
-    set "JAVA_HOME=%JRE_DIR%"
-    set "PATH=%JAVA_HOME%\bin;%PATH%"
-    "%JAVA_HOME%\bin\java" -jar "%JAR_FILE%"
+    "%JAVA_EXE%" -jar "%JAR_FILE%"
 ) else (
     echo [INFO] Checking for system Java...
     java -version >nul 2>&1
